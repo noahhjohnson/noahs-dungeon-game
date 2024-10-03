893d5e74bbe1552ec9c837bb871e0b884e1b24b1
noahhjohnson <166769527+noahhjohnson@users.noreply.github.com>
2024-10-03 19:54:45 -0400

    GameGraphics.java

--- dungeon game noah/src/GameGraphics.java
+++ dungeon game noah/src/GameGraphics.java
@@ -20,7 +20,7 @@ public class GameGraphics {
     JLabel label = new JLabel(Main.p.x + " " + Main.p.y);
     JLabel label2 = new JLabel("[Backstory]: You are a thief that has \n attempted to steal coins and you got caught. You are now serving time in prison. You have a friend, Doug who is a Prison Guard, he wants you to turn your life around.");
     JLabel label3 = new JLabel("[Doug]: I found a job opportunity for you, It's hauling crates for the Dining Hall. I'm wondering if a thief like you would even consider the position.");
-    BufferedImage pUp;
+    JPanel tutorial1 = new JPanel();
     BufferedImage prisoner;
     BufferedImage prisonWall;
     BufferedImage sink;
@@ -32,11 +32,21 @@ public class GameGraphics {
     BufferedImage StoneBrickTitle;
     BufferedImage TutBlack;
     BufferedImage TutRed;
-    BufferedImage TutBack;
+    BufferedImage PlayBack;
     BufferedImage PlayBlack;
     BufferedImage PlayRed;
     BufferedImage CobbleBack;
-    File pUpFile;
+    BufferedImage Table;
+    BufferedImage Prisoner1;
+    BufferedImage backButton;
+    BufferedImage ResumeRed;
+    BufferedImage ResumeBlack;
+
+    File ResumeBlackFile = new File("dungeon game noah/src/ResumeBlack.png");
+    File ResumeRedFile = new File("dungeon game noah/src/ResumeRed.png");
+    File Prisoner1File = new File("dungeon game noah/src/Prisoner1.png");
+    File backButtonFile = new File("dungeon game noah/src/backButton.png");
+    File TableFile = new File("dungeon game noah/src/Table.png");
     File PrisonFloorFile = new File("dungeon game noah/src/PrisonFloorTiles.png");
     File prisonerFile = new File("dungeon game noah/src/Prisoner.png");
     File PWallFile = new File("dungeon game noah/src/PrisonWall.png");
@@ -48,7 +58,7 @@ public class GameGraphics {
     File StoneBrickFile = new File("dungeon game noah/src/StoneBrickTitle.jpg");
     File TutBlackFile = new File("dungeon game noah/src/TutBlack.png");
     File TutRedFile = new File("dungeon game noah/src/TutRed.png");
-    File TutBackFile = new File("dungeon game noah/src/TutBack.png");
+    File PlayBackFile = new File("dungeon game noah/src/PlayBack.png");
     File PlayBlackFile = new File("dungeon game noah/src/PlayBlack.png");
     File PlayRedFile = new File("dungeon game noah/src/PlayRed.png");
     File CobbleBackFile = new File("dungeon game noah/src/CobbleBack.png");
@@ -71,6 +81,10 @@ public class GameGraphics {
         bed = ImageIO.read(bedFile);
         sink = ImageIO.read(sinkFile);
         floor = ImageIO.read(PrisonFloorFile);
+        Table = ImageIO.read(TableFile);
+        Prisoner1 = ImageIO.read(Prisoner1File);
+
+
 
 
         for (int i = 0; i < Map.mapRep.length; i++) {
@@ -92,6 +106,12 @@ public class GameGraphics {
                 if (Map.mapRep[j][i].equals("\uD83D\udeaa")) {
                     g.drawImage(door, j * blockSize, i * blockSize, blockSize, blockSize, null);
                 }
+                if (Map.mapRep[j][i].equals("T ")){
+                    g.drawImage(Table, j * blockSize, i * blockSize, blockSize, blockSize, null);
+                }
+                if (Map.mapRep[j][i].equals("\ud83d\ude4d")) {
+                    g.drawImage(Prisoner1, j * blockSize, i * blockSize, blockSize, blockSize, null);
+                }
             }
             g.drawImage(prisoner, Main.p.x * blockSize, Main.p.y * blockSize, blockSize, blockSize, null);
         }
@@ -104,10 +124,11 @@ public class GameGraphics {
         StoneBrickTitle = ImageIO.read(StoneBrickFile);
         TutRed = ImageIO.read(TutRedFile);
         TutBlack = ImageIO.read(TutBlackFile);
-        TutBack = ImageIO.read(TutBackFile);
+        PlayBack = ImageIO.read(PlayBackFile);
         PlayBlack = ImageIO.read(PlayBlackFile);
         PlayRed = ImageIO.read(PlayRedFile);
         CobbleBack = ImageIO.read(CobbleBackFile);
+        backButton = ImageIO.read(backButtonFile);
         g.setColor(new Color(61,93,46));
         g.fillRect(0,0, 1600, 1000);
         g.setColor(new Color(0,0,0));
@@ -120,9 +141,9 @@ public class GameGraphics {
         g.fillRect(450, 70, 700, 200);
         g.drawImage(BlackTitle, 475, 105, null);
         g.drawImage(RedTitle, 475, 110, null);
-        g.drawImage(TutBack, 450, 345, null);
-        g.drawImage(TutBack, 710, 345, null);
-        g.drawImage(TutBack, 890, 345, null);
+        g.drawImage(PlayBack, 450, 345, null);
+        g.drawImage(PlayBack, 710, 345, null);
+        g.drawImage(PlayBack, 890, 345, null);
         g.drawImage(PlayBlack, 715, 368, null);
         g.drawImage(PlayRed, 695, 383, null);
         g.drawImage(CobbleBack, 450, 600, 260,165, null);
@@ -130,7 +151,35 @@ public class GameGraphics {
         g.drawImage(CobbleBack, 970, 600, 180,165, null);
         g.drawImage(TutBlack, 640, 615, null);
         g.drawImage(TutRed, 630, 625, null);
+    }
 
+    public void drawTutorial(Graphics g) throws IOException {
+        int xPanelPosition = 40;
+        g.drawRect(145-xPanelPosition,100, 400, 650);
+        g.setColor(new Color(61, 93, 46));
+        g.fillRect(145-xPanelPosition, 100, 400, 650);
+        //-
+        g.drawRect(650-xPanelPosition, 100,400, 650);
+        g.setColor(new Color(61, 93, 46));
+        g.fillRect(650-xPanelPosition, 100, 400, 650);
+        //-
+        g.drawRect(1150-xPanelPosition,100, 400, 650);
+        g.setColor(new Color(61, 93, 46));
+        g.fillRect(1150-xPanelPosition, 100, 400, 650);
+
+        g.drawImage(backButton, 25,740, 100,100, null);
+    }
+
+    public void drawPause(Graphics g) throws IOException {
+
+        g.setColor(new Color(61,93,46));
+        g.fillRect(570, 212, 1055-570, 663-212);
+        StoneBrickTitle = ImageIO.read(StoneBrickFile);
+        PlayBack = ImageIO.read(PlayBackFile);
+        ResumeRed = ImageIO.read(ResumeRedFile);
+        ResumeBlack = ImageIO.read(ResumeBlackFile);
+        g.drawImage(StoneBrickTitle, 650, 267, 325, 150, null);
+        g.drawImage(PlayBack, 650, 450, 325, 150, null);
 
     }
 
@@ -152,12 +201,20 @@ public class GameGraphics {
                     if(mainMenuSelector.equals("Play")){
                         drawMap(g);
                     }
+
+                    if(mainMenuSelector.equals("Tutorial")){
+                        drawTutorial(g);
+                    }
                     if(isInteract){
                         g.setColor(green);
                         g.fillRect(Main.p.x * blockSize,(Main.p.y * blockSize) - blockSize, blockSize, blockSize);
                         g.fillRect(Main.p.x * blockSize,(Main.p.y * blockSize) + blockSize, blockSize, blockSize);
                         g.fillRect((Main.p.x * blockSize) - blockSize, (Main.p.y * blockSize), blockSize, blockSize);
                         g.fillRect((Main.p.x * blockSize) + blockSize, (Main.p.y * blockSize), blockSize, blockSize);
+                    }
+
+                    if(mainMenuSelector.equals("Pause Menu")){
+                        drawPause(g);
                     }
                 } catch (IOException e) {
                     throw new RuntimeException(e);
@@ -189,12 +246,22 @@ public class GameGraphics {
 
                 System.out.print("Y Position: ");
                 System.out.println(e.getY());
-                if(e.getX() > 451 && e.getX() < 1148 && e.getY() > 346 && e.getY() < 553 ){
+                if (e.getX() > 451 && e.getX() < 1148 && e.getY() > 346 && e.getY() < 536) {
                     System.out.println("Play");
                     mainMenuSelector = "Play";
+                    System.out.println(mainMenuSelector);
                     panel.repaint();
                 }
-                //if(e.getX()) =
+                if (e.getX() > 451 && e.getX() < 1148 && e.getY() > 599 && e.getY() < 763) {
+                    System.out.println("Tutorial");
+                    mainMenuSelector = "Tutorial";
+                    panel.repaint();
+                }
+                if (e.getX() > 41 && e.getX() < 105 && e.getY() > 758 && e.getY() < 820) {
+                    System.out.println("Back");
+                    mainMenuSelector = "main menu";
+                    panel.repaint();
+                }
             }
 
             @Override
@@ -246,10 +313,28 @@ public class GameGraphics {
                     // Your logic for key pressed
                     isInteract = true;
                 }
+                if(isInteract && e.getKeyCode() == KeyEvent.VK_1){
+                    BasicCommands.checkItemsNearMe(Main.p);
+                    BasicCommands.interactInput("1", Main.p, Main.m);
+                    isInteract = false;
+                }
+                if(isInteract && e.getKeyCode() == KeyEvent.VK_2){
+                    BasicCommands.checkItemsNearMe(Main.p);
+                    BasicCommands.interactInput("2", Main.p, Main.m);
+                    isInteract = false;
+                }
                 if(isInteract && e.getKeyCode() == KeyEvent.VK_3){
                     BasicCommands.checkItemsNearMe(Main.p);
                     BasicCommands.interactInput("3", Main.p, Main.m);
                     isInteract = false;
+                }
+                if(isInteract && e.getKeyCode() == KeyEvent.VK_4){
+                    BasicCommands.checkItemsNearMe(Main.p);
+                    BasicCommands.interactInput("4", Main.p, Main.m);
+                    isInteract = false;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_M){
+                    mainMenuSelector = "Pause Menu";
                 }
 
                 panel.repaint();
